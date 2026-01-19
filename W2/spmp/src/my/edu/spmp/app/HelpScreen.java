14b05aa30b9051e9cf91ccb02a7d320c91bbb155
Keith <keithkee1501@gmail.com>
2026-01-19 08:49:03 +0800

    19/1/2026

---
+++ W2/spmp/src/my/edu/spmp/app/HelpScreen.java
@@ -0,0 +1,14 @@
+package my.edu.spmp.app;
+
+import javax.swing.JFrame;
+
+public class HelpScreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javax.swing.JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+
+}
